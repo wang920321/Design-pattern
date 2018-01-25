520875e24e599cacf769049f68c55ff75aed8684
wangkaipeng <kpwang@travelsky.com>
2018-01-25 15:04:39 +0800

    工厂模式

---
+++ src/com/study/shape/Circle.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.study.shape;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Circle implements Shape {
+
+	/* (non-Javadoc)
+	 * @see com.study.factory.shape.Shape#draw()
+	 */
+	@Override
+	public void draw() {
+		System.out.println("Inside Circle::draw() method.");
+
+	}
+
+}
---
+++ src/com/study/shape/Square.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.study.shape;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Square implements Shape {
+
+	/* (non-Javadoc)
+	 * @see com.study.factory.shape.Shape#draw()
+	 */
+	@Override
+	public void draw() {
+		System.out.println("Inside Square::draw() method.");
+
+	}
+
+}
